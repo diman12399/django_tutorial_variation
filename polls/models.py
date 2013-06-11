2a01df8b7c306c49fdb263172b2d72514840c7f9
diman12399 <mdimanmail@gmail.com>
2013-06-11 19:57:44 +0000

    Added models Poll and Choice

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,14 @@
 # -*- encoding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Pool(models.Model):
+    question = models.CharField(max_length=200)
+    pun_date = models.DateTimeField('date published')
+
+
+class Chice(models.Model):
+    pool = models.ForeignKey(Pool)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
